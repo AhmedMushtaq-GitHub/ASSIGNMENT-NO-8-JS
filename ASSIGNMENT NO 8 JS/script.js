60bd29e54187deecc4d01936e2ff2e90935fc7a6
Ahmed Mushtaq <122963926+AhmedMushtaq-GitHub@users.noreply.github.com>
2023-03-06 11:29:15 +0500

    Add files via upload

---
+++ ASSIGNMENT NO 8 JS/script.js
@@ -0,0 +1,16 @@
+ // 1.	Declare two letiables: admin and name. Assign the value "Umair Azmat" to name. 
+// Copy the value from name to admin. Show the value of admin using alert (must output Name).
+
+let name = "Umair Azmat";
+let admin = name;
+alert(admin);
+
+// 2.	Create a variable with the name of our planet. How would you name such a variable?
+
+let planet = "Earth";
+
+// 3.	Create a variable to store the name of a current visitor to a website. How would you name that variable?
+
+let visitorName = "John Doe";
+
+
